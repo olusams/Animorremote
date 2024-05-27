e64e19c78fe7ba46f652221630d91ed9bb962535
Olu Sampson <60044354+olusams@users.noreply.github.com>
2024-05-27 09:23:14 +0100

    Add files via upload

--- page.jsx
+++ page.jsx
@@ -1,55 +1,88 @@
 'use client'
-import Cta from "@/app/ui/Cta"
-import Div from "@/app/ui/Div"
-import PageHeading from "@/app/ui/PageHeading"
-import Spacing from "@/app/ui/Spacing"
-import SocialWidget from "@/app/ui/Widget/SocialWidget"
-import imgUrl from '../../../../public/images/member_details_1.jpeg'
-import Image from "next/image"
+import Accordion from "@/app/ui/Accordion";
+import Button from "@/app/ui/Button";
+import Cta from "@/app/ui/Cta";
+import Div from "@/app/ui/Div";
+import PageHeading from "@/app/ui/PageHeading";
+import Spacing from "@/app/ui/Spacing";
+import { Icon } from "@iconify/react";
 
-export default function TeamDetails() {
+export default function FaqPage() {
   return (
     <>
       <PageHeading
-        title='Team Details'
-        bgSrc='/images/team_hero_bg.jpeg'
-        pageLinkText='Team Details'
+        title="Frequently Asked Questions"
+        bgSrc="/images/about_hero_bg.jpeg"
+        pageLinkText="FAQ"
       />
-      <Spacing lg='150' md='80'/>
+      <Spacing lg="150" md="80" />
       <Div className="container">
-        <Div className="row align-items-center">
-          <Div className="col-xl-5 col-lg-6">
-            <Div className="cs-radius_15 cs-shine_hover_1">
-              <Image src={imgUrl} alt="Member" className="w-100" placeholder="blur" />
+        <Div className="row">
+          <Div className="col-lg-4">
+            <Div className="cs-faq_nav cs-radius_15">
+              <h2 className="cs-faq_nav_title cs-m0">FAQ Category</h2>
+              <Div className="cs-height_30 cs-height_lg_30" />
+              <ul className="cs-list cs-style1 cs-mp0">
+                <li>
+                  <Button
+                    variant="cs-type2"
+                    btnLink="/faq"
+                    btnText="Service related"
+                    icon={
+                      <Icon icon="material-symbols:content-copy-outline-rounded" />
+                    }
+                  />
+                </li>
+                <li>
+                  <Button
+                    variant="cs-type2"
+                    btnLink="/faq"
+                    btnText="Pricing"
+                    icon={
+                      <Icon icon="material-symbols:content-copy-outline-rounded" />
+                    }
+                  />
+                </li>
+                <li>
+                  <Button
+                    variant="cs-type2"
+                    btnLink="/faq"
+                    btnText="Project delivery"
+                    icon={
+                      <Icon icon="material-symbols:content-copy-outline-rounded" />
+                    }
+                  />
+                </li>
+                <li>
+                  <Button
+                    variant="cs-type2"
+                    btnLink="/faq"
+                    btnText="Documentation"
+                    icon={
+                      <Icon icon="material-symbols:content-copy-outline-rounded" />
+                    }
+                  />
+                </li>
+              </ul>
             </Div>
           </Div>
-          <Div className="col-lg-6 offset-xl-1">
-            <Spacing lg='0' md='45'/>
-            <Div className="cs-section_heading cs-style1">
-              <h2 className="cs-section_title">Melon Bulgery</h2>
-              <Div className="cs-height_10 cs-height_lg_10" />
-              <h3 className="cs-section_subtitle">Product Designer</h3>
-              <Div className="cs-height_5 cs-height_lg_5" />
-              <Div className="cs-separator cs-accent_bg" />
-              <Div className="cs-height_45 cs-height_lg_25" />
-              <p className="cs-m0">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium voltire doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
-              <Div className="cs-height_25 cs-height_lg_20" />
-              <p className="cs-m0">Ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit.</p>
-              <Div className="cs-height_45 cs-height_lg_30" />
-              <SocialWidget/>
-            </Div>
+          <Div className="col-lg-7 offset-lg-1">
+            <Spacing lg="0" md="40" />
+            <Accordion />
           </Div>
         </Div>
       </Div>
-      <Spacing lg='150' md='80'/>
+      <Spacing lg="150" md="80" />
+      {/* Start CTA Section */}
       <Div className="container">
-        <Cta 
-          title='Let’s disscuse make <br />something <i>cool</i> together' 
-          btnText='Apply For Meeting' 
-          btnLink='/contact' 
-          bgSrc='/images/cta_bg.jpeg'
+        <Cta
+          title="Let’s disscuse make <br />something <i>cool</i> together"
+          btnText="Apply For Meeting"
+          btnLink="/contact"
+          bgSrc="/images/cta_bg.jpeg"
         />
       </Div>
+      {/* End CTA Section */}
     </>
-  )
+  );
 }
