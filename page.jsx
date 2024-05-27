dec7f76d1939b4cc73ae62b7fbd2fa6213333063
Olu Sampson <60044354+olusams@users.noreply.github.com>
2024-05-27 09:16:25 +0100

    Add files via upload

--- page.jsx
+++ page.jsx
@@ -1,289 +1,86 @@
-'use client';
-import Cta from '@/app/ui/Cta';
-import Div from '@/app/ui/Div';
-import FunFact from '@/app/ui/FunFact';
-import Hero from '@/app/ui/Hero';
-import LogoList from '@/app/ui/LogoList';
-import MovingText from '@/app/ui/MovingText';
-import SectionHeading from '@/app/ui/SectionHeading';
-import PortfolioSlider from '@/app/ui/Slider/PortfolioSlider';
-import PostSlider from '@/app/ui/Slider/PostSlider';
-import TeamSlider from '@/app/ui/Slider/TeamSlider';
-import TestimonialSlider from '@/app/ui/Slider/TestimonialSlider';
-import TimelineSlider from '@/app/ui/Slider/TimelineSlider';
-import Spacing from '@/app/ui/Spacing';
-import VideoModal from '@/app/ui/VideoModal';
-import Card from './ui/Card';
+'use client'
+import Button from "@/app/ui/Button";
+import Cta from "@/app/ui/Cta";
+import Div from "@/app/ui/Div";
+import PageHeading from "@/app/ui/PageHeading";
+import SectionHeading from "@/app/ui/SectionHeading";
+import Spacing from "@/app/ui/Spacing";
+import Image from "next/image";
+import imgUrl from '../../../../public/images/portfolio_details_1.jpeg'
 
-// Hero Social Links
-const heroSocialLinks = [
-  {
-    name: 'Behance',
-    links: '/',
-  },
-  {
-    name: 'Twitter',
-    links: '/',
-  },
-];
-// FunFact Data
-const funfaceData = [
-  {
-    title: 'Global Happy Clients',
-    factNumber: '40K',
-  },
-  {
-    title: 'Project Completed',
-    factNumber: '50K',
-  },
-  {
-    title: 'Team Members',
-    factNumber: '245',
-  },
-  {
-    title: 'Digital products',
-    factNumber: '550',
-  },
-];
-// Portfolio Data
-const portfolioData = [
-  {
-    title: 'Colorful Art Work',
-    subtitle: 'See Details',
-    href: '/portfolio/portfolio-details',
-    src: '/images/portfolio_1.jpeg',
-  },
-  {
-    title: 'Colorful Art Work',
-    subtitle: 'See Details',
-    href: '/portfolio/portfolio-details',
-    src: '/images/portfolio_2.jpeg',
-  },
-  {
-    title: 'Colorful Art Work',
-    subtitle: 'See Details',
-    href: '/portfolio/portfolio-details',
-    src: '/images/portfolio_0.jpg',
-  },
-  {
-    title: 'Colorful Art Work',
-    subtitle: 'See Details',
-    href: '/portfolio/portfolio-details',
-    src: '/images/portfolio_3.jpeg',
-  },
-];
-
-export default function Home() {
+export default function PortfolioDetailsPage() {
   return (
     <>
-      {/* Start Hero Section */}
-      <Hero
-        title="Creativity In <br/>Our Blood Line"
-        subtitle="We deliver best problem solving solution for our client and provide finest finishing product in present and upcoming future."
-        btnText="Get a Quote"
-        btnLink="/contact"
-        scrollDownId="#service"
-        socialLinksHeading="Follow Us"
-        heroSocialLinks={heroSocialLinks}
-        bgImageUrl="/images/hero_bg.jpeg"
+      <PageHeading 
+        title='Portfolio Details'
+        bgSrc='/images/service_hero_bg.jpeg'
+        pageLinkText='PORTFOLIO-DETAILS'
       />
-      {/* End Hero Section */}
-
-      {/* Start FunFact Section */}
-      <div className="container">
-        <FunFact
-          variant="cs-type1"
-          title="Our fun fact"
-          subtitle="Sed ut perspiciatis unde omnis iste natus error voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis."
-          data={funfaceData}
-        />
-      </div>
-      {/* End FunFact Section */}
-
-      {/* Start Service Section */}
-      <Spacing lg="150" md="80" />
-      <Div id="service">
-        <Div className="container">
-          <Div className="row">
-            <Div className="col-xl-4">
-              <SectionHeading
-                title="Services we can help you with"
-                subtitle="What Can We Do"
-                btnText="See All Services"
-                btnLink="/service"
-              />
-              <Spacing lg="90" md="45" />
-            </Div>
-            <Div className="col-xl-8">
-              <Div className="row">
-                <Div className="col-lg-3 col-sm-6 cs-hidden_mobile"></Div>
-                <Div className="col-lg-3 col-sm-6">
-                  <Card
-                    title="UI/UX design"
-                    link="/service/service-details"
-                    src="/images/service_1.jpeg"
-                    alt="Service"
-                  />
-                  <Spacing lg="0" md="30" />
-                </Div>
-                <Div className="col-lg-3 col-sm-6 cs-hidden_mobile"></Div>
-                <Div className="col-lg-3 col-sm-6">
-                  <Card
-                    title="React.js Development"
-                    link="/service/service-details"
-                    src="/images/service_2.jpeg"
-                    alt="Service"
-                  />
-                  <Spacing lg="0" md="30" />
-                </Div>
-                <Div className="col-lg-3 col-sm-6">
-                  <Card
-                    title="Digital Marketing"
-                    link="/service/service-details"
-                    src="/images/service_3.jpeg"
-                    alt="Service"
-                  />
-                  <Spacing lg="0" md="30" />
-                </Div>
-                <Div className="col-lg-3 col-sm-6 cs-hidden_mobile"></Div>
-                <Div className="col-lg-3 col-sm-6">
-                  <Card
-                    title="Technology"
-                    link="/service/service-details"
-                    src="/images/service_4.jpeg"
-                    alt="Service"
-                  />
-                  <Spacing lg="0" md="30" />
-                </Div>
-                <Div className="col-lg-3 col-sm-6 cs-hidden_mobile"></Div>
+      <Spacing lg='150' md='80'/>
+      <Div className="container">
+        <Image src={imgUrl} alt="Details" placeholder="blur" className="cs-radius_15 w-100" />
+        <Spacing lg='90' md='40'/>
+        <Div className="row">
+          <Div className="col-lg-6">
+            <SectionHeading 
+              title='Graffiti wall artwork' 
+              subtitle='Creative' 
+            >
+              <Spacing lg='40' md='20'/>
+              <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium voltire doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+              <Spacing lg='10' md='10'/>
+              <p>Ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit.</p>
+            </SectionHeading>
+          </Div>
+          <Div className="col-lg-5 offset-lg-1">
+            <Spacing lg='60' md='40'/>
+            <h2 className='cs-font_30 cs-font_26_sm cs-m0'>Project Info -</h2>
+            <Spacing lg='50' md='30'/>
+            <Div className="row">
+              <Div className="col-6">
+                <h3 className='cs-accent_color cs-font_22 cs-font_18_sm cs-m0'>Category:</h3>
+                <p className='cs-m0'>Artwork</p>
+                <Spacing lg='30' md='30'/>
+              </Div>
+              <Div className="col-6">
+                <h3 className='cs-accent_color cs-font_22 cs-font_18_sm cs-m0'>Location:</h3>
+                <p className='cs-m0'>United Kindom</p>
+                <Spacing lg='30' md='30'/>
+              </Div>
+              <Div className="col-6">
+                <h3 className='cs-accent_color cs-font_22 cs-font_18_sm cs-m0'>Software:</h3>
+                <p className='cs-m0'>Adobe Illustrator</p>
+                <Spacing lg='30' md='30'/>
+              </Div>
+              <Div className="col-6">
+                <h3 className='cs-accent_color cs-font_22 cs-font_18_sm cs-m0'>Dated:</h3>
+                <p className='cs-m0'>14-Aug-2022</p>
+                <Spacing lg='30' md='30'/>
+              </Div>
+              <Div className="col-6">
+                <h3 className='cs-accent_color cs-font_22 cs-font_18_sm cs-m0'>Client:</h3>
+                <p className='cs-m0'>Andreo Bowla</p>
+                <Spacing lg='30' md='30'/>
               </Div>
             </Div>
           </Div>
         </Div>
-      </Div>
-      {/* End Service Section */}
-
-      {/* Start Portfolio Section */}
-      <Spacing lg="150" md="50" />
-      <Div>
-        <Div className="container">
-          <SectionHeading
-            title="Portfolio to explore"
-            subtitle="Latest Projects"
-            variant="cs-style1 text-center"
-          />
-          <Spacing lg="90" md="45" />
-        </Div>
-        <PortfolioSlider data={portfolioData} />
-      </Div>
-      {/* End Portfolio Section */}
-
-      {/* Start Awards Section */}
-      <Spacing lg="150" md="80" />
-      <Div className="cs-shape_wrap_2">
-        <Div className="cs-shape_2">
-          <Div />
-        </Div>
-        <Div className="container">
-          <Div className="row">
-            <Div className="col-xl-4">
-              <SectionHeading
-                title="We get multiple awards"
-                subtitle="Our Awards"
-                variant="cs-style1"
-              />
-              <Spacing lg="90" md="45" />
+        <Spacing lg='65' md='10'/>
+          <Div className="cs-page_navigation cs-center">
+            <Div>
+              <Button btnLink='/portfolio/portfolio-details' btnText='Prev Project' variant='cs-type1'/>
             </Div>
-            <Div className="col-xl-7 offset-xl-1">
-              <TimelineSlider />
+            <Div>
+              <Button btnLink='/portfolio/portfolio-details' btnText='Next Project'/>
             </Div>
           </Div>
-        </Div>
       </Div>
-      {/* End Awards Section */}
-
-      {/* Start Video Block Section */}
-      <Spacing lg="130" md="70" />
-      <Div className="container">
-        <h2 className="cs-font_50 cs-m0 text-center cs-line_height_4">
-          Our agile process is ability to adapt and respond to change. Agile
-          organizations view change as an opportunity, not a threat.
-        </h2>
-        <Spacing lg="70" md="70" />
-        <VideoModal
-          videoSrc="https://www.youtube.com/watch?v=VcaAVWtP48A"
-          bgUrl="/images/video_bg.jpeg"
-        />
-      </Div>
-      {/* End Video Block Section */}
-
-      {/* Start Team Section */}
-      <Spacing lg="145" md="80" />
-      <Div className="container">
-        <SectionHeading
-          title="Awesome team <br/>members"
-          subtitle="Our Team"
-          variant="cs-style1"
-        />
-        <Spacing lg="85" md="45" />
-        <TeamSlider />
-      </Div>
-      <Spacing lg="150" md="80" />
-      {/* End Team Section */}
-
-      {/* Start Testimonial Section */}
-      <TestimonialSlider />
-      {/* End Testimonial Section */}
-
-      {/* Start Blog Section */}
-      <Spacing lg="150" md="80" />
-      <Div className="cs-shape_wrap_4">
-        <Div className="cs-shape_4"></Div>
-        <Div className="cs-shape_4"></Div>
-        <Div className="container">
-          <Div className="row">
-            <Div className="col-xl-4">
-              <SectionHeading
-                title="Explore recent publication"
-                subtitle="Our Blog"
-                btnText="View More Blog"
-                btnLink="/blog"
-              />
-              <Spacing lg="90" md="45" />
-            </Div>
-            <Div className="col-xl-7 offset-xl-1">
-              <Div className="cs-half_of_full_width">
-                <PostSlider />
-              </Div>
-            </Div>
-          </Div>
-        </Div>
-      </Div>
-      {/* End Blog Section */}
-
-      {/* Start MovingText Section */}
-      <Spacing lg="125" md="70" />
-      <MovingText text="Our reputed world wide partners" />
-      <Spacing lg="105" md="70" />
-      {/* End MovingText Section */}
-
-      {/* Start LogoList Section */}
-      <Div className="container">
-        <LogoList />
-      </Div>
-      <Spacing lg="150" md="80" />
-      {/* End LogoList Section */}
-
-      {/* Start CTA Section */}
-      <Div className="container">
-        <Cta
-          title="Let’s disscuse make <br />something <i>cool</i> together"
-          btnText="Apply For Meeting"
-          btnLink="/contact"
-          bgSrc="/images/cta_bg.jpeg"
-        />
-      </Div>
-      {/* End CTA Section */}
+      <Spacing lg='145' md='80'/>
+      <Cta 
+        title='agency@arino.com' 
+        bgSrc='/images/cta_bg_2.jpeg'
+        variant='rounded-0'
+      />
     </>
-  );
+  )
 }
