d209d97d29af49e172806052754738f5c8b39ffe
Olu Sampson <60044354+olusams@users.noreply.github.com>
2024-05-27 09:21:28 +0100

    Add files via upload

--- page.jsx
+++ page.jsx
@@ -1,19 +1,21 @@
-'use client'
-import { useState } from "react";
-import Cta from "@/app/ui/Cta";
-import Div from "@/app/ui/Div";
-import Hero8 from "@/app/ui/Hero/Hero8";
-import LogoList from "@/app/ui/LogoList";
-import MovingText from "@/app/ui/MovingText";
-import Portfolio from "@/app/ui/Portfolio";
-import SectionHeading from "@/app/ui/SectionHeading";
-import ServiceListStyle2 from "@/app/ui/ServiceList/ServiceListStyle2";
-import TestimonialSlider from "@/app/ui/Slider/TestimonialSlider";
-import Spacing from "@/app/ui/Spacing";
-import VideoModal from "@/app/ui/VideoModal";
-import { Icon } from "@iconify/react";
+'use client';
+import Cta from '@/app/ui/Cta';
+import Div from '@/app/ui/Div';
+import FunFact from '@/app/ui/FunFact';
+import Hero from '@/app/ui/Hero';
+import LogoList from '@/app/ui/LogoList';
+import MovingText from '@/app/ui/MovingText';
+import SectionHeading from '@/app/ui/SectionHeading';
+import PortfolioSlider from '@/app/ui/Slider/PortfolioSlider';
+import PostSlider from '@/app/ui/Slider/PostSlider';
+import TeamSlider from '@/app/ui/Slider/TeamSlider';
+import TestimonialSlider from '@/app/ui/Slider/TestimonialSlider';
+import TimelineSlider from '@/app/ui/Slider/TimelineSlider';
+import Spacing from '@/app/ui/Spacing';
+import VideoModal from '@/app/ui/VideoModal';
+import Card from './ui/Card';
 
-
+// Hero Social Links
 const heroSocialLinks = [
   {
     name: 'Behance',
@@ -24,205 +26,184 @@ const heroSocialLinks = [
     links: '/',
   },
 ];
+// FunFact Data
+const funfaceData = [
+  {
+    title: 'Global Happy Clients',
+    factNumber: '40K',
+  },
+  {
+    title: 'Project Completed',
+    factNumber: '50K',
+  },
+  {
+    title: 'Team Members',
+    factNumber: '245',
+  },
+  {
+    title: 'Digital products',
+    factNumber: '550',
+  },
+];
+// Portfolio Data
 const portfolioData = [
   {
     title: 'Colorful Art Work',
     subtitle: 'See Details',
     href: '/portfolio/portfolio-details',
-    src: '/images/portfolio_39.jpeg',
-    category: 'ui_ux_design',
+    src: '/images/portfolio_1.jpeg',
   },
   {
     title: 'Colorful Art Work',
     subtitle: 'See Details',
     href: '/portfolio/portfolio-details',
-    src: '/images/portfolio_40.jpeg',
-    category: 'logo_design',
+    src: '/images/portfolio_2.jpeg',
   },
   {
     title: 'Colorful Art Work',
     subtitle: 'See Details',
     href: '/portfolio/portfolio-details',
-    src: '/images/portfolio_41.jpeg',
-    category: 'web_design',
+    src: '/images/portfolio_0.jpg',
   },
   {
     title: 'Colorful Art Work',
     subtitle: 'See Details',
     href: '/portfolio/portfolio-details',
-    src: '/images/portfolio_42.jpeg',
-    category: 'mobile_apps',
-  },
-  {
-    title: 'Colorful Art Work',
-    subtitle: 'See Details',
-    href: '/portfolio/portfolio-details',
-    src: '/images/portfolio_43.jpeg',
-    category: 'ui_ux_design',
-  },
-  {
-    title: 'Colorful Art Work',
-    subtitle: 'See Details',
-    href: '/portfolio/portfolio-details',
-    src: '/images/portfolio_44.jpeg',
-    category: 'web_design',
-  },
-  {
-    title: 'Colorful Art Work',
-    subtitle: 'See Details',
-    href: '/portfolio/portfolio-details',
-    src: '/images/portfolio_10.jpeg',
-    category: 'logo_design',
-  },
-  {
-    title: 'Colorful Art Work',
-    subtitle: 'See Details',
-    href: '/portfolio/portfolio-details',
-    src: '/images/portfolio_4.jpeg',
-    category: 'ui_ux_design',
-  },
-  {
-    title: 'Colorful Art Work',
-    subtitle: 'See Details',
-    href: '/portfolio/portfolio-details',
-    src: '/images/portfolio_5.jpeg',
-    category: 'logo_design',
-  },
-  {
-    title: 'Colorful Art Work',
-    subtitle: 'See Details',
-    href: '/portfolio/portfolio-details',
-    src: '/images/portfolio_6.jpeg',
-    category: 'web_design',
-  },
-];
-const categoryMenu = [
-  {
-    title: 'Web Design',
-    category: 'web_design',
-  },
-  {
-    title: 'UI/UX Design',
-    category: 'ui_ux_design',
-  },
-  {
-    title: 'Mobile Apps',
-    category: 'mobile_apps',
-  },
-  {
-    title: 'Logo Design',
-    category: 'logo_design',
+    src: '/images/portfolio_3.jpeg',
   },
 ];
 
-export default function FreelancerAgencyHome() {
-  const [active, setActive] = useState('all');
-  const [itemShow, setItemShow] = useState(6);
+export default function Home() {
   return (
     <>
-      <Hero8
-        title="We Are Award’s Wining Freelancing Agency"
-        subtitle="We deliver best problem solving solution for our client and provide
-        finest finishing product in present and upcoming future."
+      {/* Start Hero Section */}
+      <Hero
+        title="Creativity In <br/>Our Blood Line"
+        subtitle="We deliver best problem solving solution for our client and provide finest finishing product in present and upcoming future."
+        btnText="Get a Quote"
         btnLink="/contact"
-        btnText="Let’s talk"
+        scrollDownId="#service"
         socialLinksHeading="Follow Us"
         heroSocialLinks={heroSocialLinks}
-        bgImageUrl="/images/hero_bg_5.jpeg"
-        bannerHighlightText="A"
-        spiningCircleUrl="/images/hero_img.svg"
+        bgImageUrl="/images/hero_bg.jpeg"
       />
-      <section className="cs-shape_wrap_4 cs-parallax">
-        <div className="cs-shape_4 cs-to_up" />
-        <div className="cs-shape_4 cs-to_right" />
-        <Spacing lg="145" md="80" />
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-5 col-xl-4">
+      {/* End Hero Section */}
+
+      {/* Start FunFact Section */}
+      <div className="container">
+        <FunFact
+          variant="cs-type1"
+          title="Our fun fact"
+          subtitle="Sed ut perspiciatis unde omnis iste natus error voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis."
+          data={funfaceData}
+        />
+      </div>
+      {/* End FunFact Section */}
+
+      {/* Start Service Section */}
+      <Spacing lg="150" md="80" />
+      <Div id="service">
+        <Div className="container">
+          <Div className="row">
+            <Div className="col-xl-4">
               <SectionHeading
-                title="We provide best value offer"
-                subtitle="Services"
-                btnLink="/service"
+                title="Services we can help you with"
+                subtitle="What Can We Do"
                 btnText="See All Services"
+                btnLink="/service"
               />
-              <Spacing lg="45" md="45" />
-            </div>
-            <div className="col-lg-7 offset-xl-1">
-              <ServiceListStyle2 />
-            </div>
-          </div>
-        </div>
-      </section>
-      <Spacing lg="115" md="55" />
-      <Div className="container">
-        <Div className="cs-portfolio_1_heading">
-          <SectionHeading title="Some recent work" subtitle="Our Portfolio" />
-          <Div className="cs-filter_menu cs-style1">
-            <ul className="cs-mp0 cs-center">
-              <li className={active === 'all' ? 'active' : ''}>
-                <span onClick={() => setActive('all')}>All</span>
-              </li>
-              {categoryMenu.map((item, index) => (
-                <li
-                  className={active === item.category ? 'active' : ''}
-                  key={index}
-                >
-                  <span onClick={() => setActive(item.category)}>
-                    {item.title}
-                  </span>
-                </li>
-              ))}
-            </ul>
+              <Spacing lg="90" md="45" />
+            </Div>
+            <Div className="col-xl-8">
+              <Div className="row">
+                <Div className="col-lg-3 col-sm-6 cs-hidden_mobile"></Div>
+                <Div className="col-lg-3 col-sm-6">
+                  <Card
+                    title="UI/UX design"
+                    link="/service/service-details"
+                    src="/images/service_1.jpeg"
+                    alt="Service"
+                  />
+                  <Spacing lg="0" md="30" />
+                </Div>
+                <Div className="col-lg-3 col-sm-6 cs-hidden_mobile"></Div>
+                <Div className="col-lg-3 col-sm-6">
+                  <Card
+                    title="React.js Development"
+                    link="/service/service-details"
+                    src="/images/service_2.jpeg"
+                    alt="Service"
+                  />
+                  <Spacing lg="0" md="30" />
+                </Div>
+                <Div className="col-lg-3 col-sm-6">
+                  <Card
+                    title="Digital Marketing"
+                    link="/service/service-details"
+                    src="/images/service_3.jpeg"
+                    alt="Service"
+                  />
+                  <Spacing lg="0" md="30" />
+                </Div>
+                <Div className="col-lg-3 col-sm-6 cs-hidden_mobile"></Div>
+                <Div className="col-lg-3 col-sm-6">
+                  <Card
+                    title="Technology"
+                    link="/service/service-details"
+                    src="/images/service_4.jpeg"
+                    alt="Service"
+                  />
+                  <Spacing lg="0" md="30" />
+                </Div>
+                <Div className="col-lg-3 col-sm-6 cs-hidden_mobile"></Div>
+              </Div>
+            </Div>
           </Div>
         </Div>
-        <Spacing lg="90" md="45" />
-        <Div className="cs_portfolio_grid_2">
-          {portfolioData.slice(0, itemShow).map((item, index) => (
-            <Div
-              className={`${
-                active === 'all'
-                  ? ''
-                  : !(active === item.category)
-                  ? 'd-none'
-                  : ''
-              }`}
-              key={index}
-            >
-              <Portfolio
-                title={item.title}
-                subtitle={item.subtitle}
-                href={item.href}
-                src={item.src}
-                variant="cs-style1 cs-type1"
-              />
-            </Div>
-          ))}
-        </Div>
+      </Div>
+      {/* End Service Section */}
 
-        <Div className="text-center">
-          {portfolioData.length <= itemShow ? (
-            ''
-          ) : (
-            <>
-              <Spacing lg="65" md="40" />
-              <span
-                className="cs-text_btn"
-                onClick={() => setItemShow(itemShow + 4)}
-              >
-                <span>Load More</span>
-                <Icon icon="bi:arrow-right" />
-              </span>
-            </>
-          )}
+      {/* Start Portfolio Section */}
+      <Spacing lg="150" md="50" />
+      <Div>
+        <Div className="container">
+          <SectionHeading
+            title="Portfolio to explore"
+            subtitle="Latest Projects"
+            variant="cs-style1 text-center"
+          />
+          <Spacing lg="90" md="45" />
+        </Div>
+        <PortfolioSlider data={portfolioData} />
+      </Div>
+      {/* End Portfolio Section */}
+
+      {/* Start Awards Section */}
+      <Spacing lg="150" md="80" />
+      <Div className="cs-shape_wrap_2">
+        <Div className="cs-shape_2">
+          <Div />
+        </Div>
+        <Div className="container">
+          <Div className="row">
+            <Div className="col-xl-4">
+              <SectionHeading
+                title="We get multiple awards"
+                subtitle="Our Awards"
+                variant="cs-style1"
+              />
+              <Spacing lg="90" md="45" />
+            </Div>
+            <Div className="col-xl-7 offset-xl-1">
+              <TimelineSlider />
+            </Div>
+          </Div>
         </Div>
       </Div>
-      <Spacing lg="145" md="80" />
-      {/* Start Testimonial Section */}
-      <TestimonialSlider />
-      {/* End Testimonial Section */}
+      {/* End Awards Section */}
 
       {/* Start Video Block Section */}
-      <Spacing lg="140" md="70" />
+      <Spacing lg="130" md="70" />
       <Div className="container">
         <h2 className="cs-font_50 cs-m0 text-center cs-line_height_4">
           Our agile process is ability to adapt and respond to change. Agile
@@ -231,10 +212,54 @@ export default function FreelancerAgencyHome() {
         <Spacing lg="70" md="70" />
         <VideoModal
           videoSrc="https://www.youtube.com/watch?v=VcaAVWtP48A"
-          bgUrl="/images/video_bg_2.jpeg"
+          bgUrl="/images/video_bg.jpeg"
         />
       </Div>
       {/* End Video Block Section */}
+
+      {/* Start Team Section */}
+      <Spacing lg="145" md="80" />
+      <Div className="container">
+        <SectionHeading
+          title="Awesome team <br/>members"
+          subtitle="Our Team"
+          variant="cs-style1"
+        />
+        <Spacing lg="85" md="45" />
+        <TeamSlider />
+      </Div>
+      <Spacing lg="150" md="80" />
+      {/* End Team Section */}
+
+      {/* Start Testimonial Section */}
+      <TestimonialSlider />
+      {/* End Testimonial Section */}
+
+      {/* Start Blog Section */}
+      <Spacing lg="150" md="80" />
+      <Div className="cs-shape_wrap_4">
+        <Div className="cs-shape_4"></Div>
+        <Div className="cs-shape_4"></Div>
+        <Div className="container">
+          <Div className="row">
+            <Div className="col-xl-4">
+              <SectionHeading
+                title="Explore recent publication"
+                subtitle="Our Blog"
+                btnText="View More Blog"
+                btnLink="/blog"
+              />
+              <Spacing lg="90" md="45" />
+            </Div>
+            <Div className="col-xl-7 offset-xl-1">
+              <Div className="cs-half_of_full_width">
+                <PostSlider />
+              </Div>
+            </Div>
+          </Div>
+        </Div>
+      </Div>
+      {/* End Blog Section */}
 
       {/* Start MovingText Section */}
       <Spacing lg="125" md="70" />
